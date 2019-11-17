f9d792ae3083f846fb000fab0060d95e1ba17eb6
Anwesh43 <anweshthecool0@gmail.com>
2019-11-17 16:43:17 +0530

    adding SquareLineShrink class

--- squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
+++ squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
@@ -168,4 +168,28 @@ class SquareLineShrinkView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareLineShrink(var i : Int) {
+
+        private val root : SLSNode = SLSNode(0)
+        private var curr : SLSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun starttUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
