92ab43838692f73b24688d2537d30df893d3961b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-17 16:50:31 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarelineshrinkview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarelineshrinkview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarelineshrinkview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarelineshrinkview.SquareLineShrinkView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareLineShrinkView.create(this)
     }
 }
