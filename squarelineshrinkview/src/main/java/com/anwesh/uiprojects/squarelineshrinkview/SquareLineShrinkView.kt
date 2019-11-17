a1071d104f8fe76ce6c825d95422b6fd9100c564
Anwesh43 <anweshthecool0@gmail.com>
2019-11-17 16:35:12 +0530

    adding SquareLineShrinkView class

--- squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
+++ squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
@@ -61,3 +61,21 @@ fun Canvas.drawSLSNode(i : Int, scale : Float, paint : Paint) {
     drawSquaresLineShrink(scale, size, paint)
     restore()
 }
+
+class SquareLineShrinkView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
