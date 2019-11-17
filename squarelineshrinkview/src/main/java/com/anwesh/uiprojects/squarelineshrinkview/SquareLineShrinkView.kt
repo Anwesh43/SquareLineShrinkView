e0da23e1114f6e2db492bef9eb25bd2047d171ad
Anwesh43 <anweshthecool0@gmail.com>
2019-11-17 16:24:01 +0530

    adding cosntants and libraries

--- squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
+++ squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squarelineshrinkview
 /**
  * Created by anweshmishra on 17/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val squares : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
+val scGap : Float = 0.02f
