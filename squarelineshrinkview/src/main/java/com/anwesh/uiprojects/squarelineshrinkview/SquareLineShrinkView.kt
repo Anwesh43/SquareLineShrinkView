ccebd7362f6add0222496a95f63f642f8fe76471
Anwesh43 <anweshthecool0@gmail.com>
2019-11-17 16:41:08 +0530

    adding SLSNode class

--- squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
+++ squarelineshrinkview/src/main/java/com/anwesh/uiprojects/squarelineshrinkview/SquareLineShrinkView.kt
@@ -126,4 +126,46 @@ class SquareLineShrinkView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLSNode(var i : Int, val state : State = State()) {
+
+        private var next : SLSNode? = null
+        private var prev : SLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLSNode {
+            var curr : SLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
